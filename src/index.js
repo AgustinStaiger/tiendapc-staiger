ae63929fc34dab71e007b409fc531127f7ec806a
Agustin Staiger <agustinstaiger_155@hotmail.com>
2022-06-20 17:16:06 -0300

    Creacion carpeta Components

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+
+import 'bootstrap';
+
+// or get all of the named exports for further usage
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
